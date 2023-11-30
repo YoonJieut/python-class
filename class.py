1b20899ad868b7fcd439756b560280c77644d343
YoonJieut <qmdldos@gmail.com>
2023-12-01 00:42:32 +0900

    feat : Calculator class 작성해보기

--- class.py
+++ class.py
@@ -1,10 +1,27 @@
 result = 0
 
-def add(num) :
+def add1(num) :
   global result
   result += num
   return result
 
 
-print(add(3));
-print(add(4));
+# print(add1(3));
+# print(add1(4));
+
+
+class Calculator :
+  def __init__(self):
+    self.result = 0
+  
+  def add(self, num):
+    self.result += num
+    return self.result
+  
+cal1 = Calculator()
+cal2 = Calculator()
+
+
+print(cal1.add(3))
+print(cal2.add(45))
+
